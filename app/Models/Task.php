721ba88ab2346a77588d93cc8b90ce88b707d589
jonathanmenichetti <jonathan.menichetti@gmail.com>
2025-12-01 11:53:52 -0300

    Add Task model and migration for task table.

---
+++ app/Models/Task.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'category',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
