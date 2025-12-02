5a20c0f6d61ef55f0e0bfe2db2e55078c5ab2ef9
jonathanmenichetti <jonathan.menichetti@gmail.com>
2025-12-01 22:32:21 -0300

    Task routes with default controller.

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::prefix('tasks')->group(function () {
-  Route::get('/', [TaskController::class, 'index']);
-  Route::post('/', [TaskController::class, 'store']);
-  Route::get('/{id}', [TaskController::class, 'show']);
-  Route::put('/{id}', [TaskController::class, 'update']);
-  Route::delete('/{id}', [TaskController::class, 'destroy']);
-});
+Route::controller(TaskController::class)
+  ->prefix('tasks')
+  ->group(function () {
+    Route::get('', 'index');
+    Route::post('', 'store');
+    Route::put('{id}', 'update');
+    Route::delete('{id}', 'destroy');
+  });
